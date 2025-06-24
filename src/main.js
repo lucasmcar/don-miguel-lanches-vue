e5b82346464fd93e1d16f35c279de7b9aa8adde6
lucasmcar <lmc.lucas90@gmail.com>
2025-06-23 23:04:58 -0300

    Added robots, indexed admin and  netlify e fix metatags

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '../router/routes'
+import Vue from "vue";
+import VueMeta from "vue-meta";
+
+Vue.use(VueMeta);
 
 const app = createApp(App);
 
