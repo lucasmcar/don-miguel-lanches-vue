1601a21ee979f6438e6c93b85da4a32c83cd97c9
lucasmcar <lmc.lucas90@gmail.com>
2025-06-23 23:07:30 -0300

    removed failed methods

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '../router/routes'
-import Vue from "vue";
-import VueMeta from "vue-meta";
 
-Vue.use(VueMeta);
 
 const app = createApp(App);
 
